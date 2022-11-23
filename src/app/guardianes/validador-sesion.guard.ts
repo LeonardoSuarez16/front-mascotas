09cacc7cd206b0e5e13d32b63d00e1b852b7abad
Leonardo Suarez Restrepo <leonardosuarez16@hotmail.com>
2022-11-23 06:43:43 -0500

    quitamos el error 2

--- src/app/guardianes/validador-sesion.guard.ts
+++ src/app/guardianes/validador-sesion.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { RootQuerySelector } from 'mongoose';
+// import { RootQuerySelector } from 'mongoose';
 import { Observable } from 'rxjs';
 import { SeguridadService } from '../servicios/seguridad.service';
 
